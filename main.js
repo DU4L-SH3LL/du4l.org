803750bdcb15980e9ae7b1ffe01085992e18980a
Frederik Reiter <kontakt@frederikreiter.de>
2022-10-19 21:41:11 +0200

    Fix incorrect sleep
    Previously if charArray.length * 100 > sleepTime,
    then the text starts disappearing before being fully shown.
    Add the required sleep time to the sleep calls.

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ function sleep(ms) {
 
 const textArray = ['SH3LL', 'R3V3R53', 'W3B', 'CRYP70', 'P4WN', '@DHBW Mannheim'];
 const elementId = typewriterText;
-const sleepTime = 2000;
+const charTime = 100; // ms
+const sleepTime = 2000; // ms
 
 (async function typeWriter() {
 	for (let i = 0; i < textArray.length; i++) {
@@ -15,19 +16,21 @@ const sleepTime = 2000;
 		let charArray = textArray[i].split('');
 
 		charArray.forEach((letter, j) => 
-			setTimeout(() => (elementId.innerHTML += letter), 100 * j)
+			setTimeout(() => (elementId.innerHTML += letter), charTime * j)
 		);
-
-		await sleep(sleepTime);
+		
+		// need to wait for all characters to appear, then sleep
+		let totalSleepTime = sleepTime + charArray.length * charTime;
+		await sleep(totalSleepTime); 
 
 		charArray.forEach((letter, j) => {
 				setTimeout(() => {
 					elementId.innerHTML = elementId.innerHTML.slice(0, -1); 
-				}, 100 * j);
+				}, charTime * j);
 			}
 		);
 
-		await sleep(sleepTime);
+		await sleep(totalSleepTime);
 	}
 
 	setTimeout(typeWriter, sleepTime);
